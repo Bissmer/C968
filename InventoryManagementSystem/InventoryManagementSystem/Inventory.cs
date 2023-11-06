d15e40587af40e32d704086fd30e960b7a0dd268
Artsiom Y <art.yesis@live.com>
2023-11-05 22:26:18 -0800

    added Product Class, started to make an Inventory class

---
+++ InventoryManagementSystem/InventoryManagementSystem/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace InventoryManagementSystem
+{
+    public class Inventory
+    {
+        private static BindingList<Product> products = new BindingList<Product>();
+        private static BindingList<Part> allParts = new BindingList<Part>();
+
+        public static BindingList<Product> getProducts()
+        {
+            return products;
+        }
+
+        public static BindingList<Part> getAllParts()
+        {
+            return allParts;
+        }
+
+    }
+}
---
+++ InventoryManagementSystem/InventoryManagementSystem/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class Product
+    {
+        private BindingList<Part> associatedParts = new BindingList<Part>();
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+        public int Min { get; set; }
+        public int Max { get; set; }
+
+
+        public BindingList<Part> getAssociatedParts()
+        {
+            return associatedParts;
+        }
+
+        public void addAssociatedPart(Part part)
+        {
+            associatedParts.Add(part);
+        }
+
+        public bool removeAssociatedPart(int partId)
+        {
+            bool removed = false;
+            Part partToRemove = lookupAssociatedPart(partId);
+            foreach (Part part in associatedParts)
+            {
+                if (partToRemove == null)
+                {
+                    removed = false;
+                }
+                else
+                {
+                    associatedParts.Remove(partToRemove);
+                    removed = true;
+                }
+            }
+            return removed;
+        }
+
+        public Part lookupAssociatedPart(int partId)
+        {
+            foreach (Part part in associatedParts)
+            {
+                if (part.PartId == partId)
+                {
+                    return part;
+                }
+            }
+            return null;
+        }
+    }
+}
